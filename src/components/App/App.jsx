9884ac1665a66c7a659f71ac95fed7bde170a5b3
Your Name <wtm@hotmail.com.au>
2023-02-20 18:44:36 +0200

    second final

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,7 +1,7 @@
 import { Component } from 'react';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import PropTypes from 'prop-types'
+
 
 import { Modal } from 'components/Modal/Modal';
 import { SearchBar } from 'components/Searchbar/Searchbar';
